ce4fd9ae902b3dede2e747f39a8e113d2207a652
mackyclemen <mackyclemen@gmail.com>
2020-02-22 22:55:03 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## BlueHacks19 Repo
---
+++ app/src/main/java/tech/codeverse/philcu/LaunchActivity.java
@@ -0,0 +1,19 @@
+package tech.codeverse.philcu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.widget.VideoView;
+
+public class LaunchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+
+    }
+
+
+}
